f7059cf81ecec114e3914a8baa7a3e83a318e995
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-11-17 23:20:37 +0100

    Implement Display for all types.

--- src/ast.rs
+++ src/ast.rs
@@ -53,6 +53,12 @@ impl PartialEq for ParamDecl {
 
 impl Eq for ParamDecl {}
 
+impl ParamDecl {
+    pub fn typ_as_string(&self) -> String {
+        self.typ.to_token_stream().to_string()
+    }
+}
+
 /// A declaration of the predicate.
 ///
 /// ```plain
@@ -92,6 +98,15 @@ pub enum Arg {
     Wildcard,
 }
 
+impl fmt::Display for Arg {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Arg::Ident(ident) => write!(f, "{}", ident),
+            Arg::Wildcard => write!(f, "_"),
+        }
+    }
+}
+
 /// A richer type of atom, which can be negated, and used as
 /// premises/hypotheses in rules.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -99,6 +114,25 @@ pub struct Literal {
     pub is_negated: bool,
     pub predicate: Ident,
     pub args: Vec<Arg>,
+}
+
+impl fmt::Display for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.is_negated {
+            write!(f, "!")?;
+        }
+        write!(f, "{}(", self.predicate)?;
+        let mut first = true;
+        for arg in &self.args {
+            if first {
+                first = false;
+            } else {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", arg)?;
+        }
+        write!(f, ")")
+    }
 }
 
 /// A head of a rule.
@@ -133,6 +167,22 @@ impl fmt::Display for RuleHead {
 pub struct Rule {
     pub head: RuleHead,
     pub body: Vec<Literal>,
+}
+
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} :- ", self.head)?;
+        let mut first = true;
+        for literal in &self.body {
+            if first {
+                first = false;
+            } else {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", literal)?;
+        }
+        write!(f, ".")
+    }
 }
 
 /// A Datalog program.
